c81ba8b4f528ff79c95eecbc043f43b3957d3fdd
Allan Farca <allan.farca@gmail.com>
2020-03-30 03:43:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,18 +24,6 @@ def assign_rooms(attendees)
 end
 
 def printer (attendees)
-  badges_and_room_assignments = []
-  attendees.each do |attendee|
-     badges_and_room_assignments << "Hello, my name is #{attendee}."
-  end
-  attendees.each do |attendee|
-    room_number = attendees.index(attendee) + 1
-    badges_and_room_assignments <<  "Hello, #{attendee}! You'll be assigned to room #{room_number}!"
-  end
-  badges_and_room_assignments
-end
-
-def printer (attendees)
   batch_badge_creator(attendees).each do |printing|
     puts printing
   end
